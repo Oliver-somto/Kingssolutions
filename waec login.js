ed0e9b6b3e863ab6c885fe09f613165a034c8b46
Oliver-somto <152724173+Oliver-somto@users.noreply.github.com>
2023-12-09 23:51:04 +0100

    Add files via upload

---
+++ waec login.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form")
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    
+    const pin = form.pin.value
+    
+    const authenticated =authentication(pin)
+    
+    if(authenticated){
+        window.location.href ="WAEC answer.html"
+    }else{
+        alert("Invalid pin")
+    }
+})
+
+//function for checking pin
+
+function authentication(pin){
+    if(pin==="123456"){
+        return true
+    }else{return false
+        
+    }
+    
+}
